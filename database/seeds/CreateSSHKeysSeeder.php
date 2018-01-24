693a5016f25d40b0c55f04a3ac9ceb3ef69359e0
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-25 04:00:30 +0900

    シーダー作成

--- database/seeds/CreateSSHKeysSeeder.php
+++ database/seeds/CreateSSHKeysSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CreateSSHKeysSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call('CreateSSHKeysSeeder');
+        DB::table('ssh_keys')->insert([
+            'keys' => 'test key'
+        ]);
     }
 }
