663267ce737f1772e6f949c85fa24528d89c35b9
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-27 13:19:34 +0900

    チェック用コマンドを実装

--- app/Console/Commands/CheckKey.php
+++ app/Console/Commands/CheckKey.php
@@ -27,6 +27,39 @@ class CheckKey extends Command {
      * @return void
      */
     public function handle() {
-        print 'test';
+        $max = DB::table('ssh_keys')->max('id');
+        $response = DB::table('ssh_keys')->get();
+        $cnt = [
+            'ssh-rsa' => 0,
+            'ssh-dss' => 0,
+            'ecdsa-sha2-nistp256' => 0,
+            'ecdsa-sha2-nistp384' => 0,
+            'ssh-ed25519' => 0
+        ];
+        for ($i = 1; $i <= $max - 1; $i++) {
+            switch ($response[$i]->key) {
+                case 'ssh-rsa':
+                    $cnt['ssh-rsa']++;
+                    break;
+                case 'ssh-dss':
+                    $cnt['ssh-dss']++;
+                    break;
+                case 'ecdsa-sha2-nistp256':
+                    $cnt['ecdsa-sha2-nistp256']++;
+                    break;
+                case 'ecdsa-sha2-nistp384':
+                    $cnt['ecdsa-sha2-nistp384']++;
+                    break;
+                case 'ssh-ed25519':
+                    $cnt['ssh-ed25519']++;
+                    break;
+            }
+        }
+
+        echo 'ssh-rsa: ' . round($cnt['ssh-rsa'] / ($max - 1) * 100, 2) . "%\n";
+        echo 'ssh-dss: ' . round($cnt['ssh-dss'] / ($max - 1) * 100, 2) . "%\n";
+        echo 'ecdsa-sha2-nistp256: ' . round($cnt['ecdsa-sha2-nistp256'] / ($max - 1) * 100, 2) . "%\n";
+        echo 'ecdsa-sha2-nistp384: ' . round($cnt['ecdsa-sha2-nistp384'] / ($max - 1) * 100, 2) . "%\n";
+        echo 'ssh-ed25519: ' . round($cnt['ssh-ed25519'] / ($max - 1) * 100) . "%\n";
     }
 }
